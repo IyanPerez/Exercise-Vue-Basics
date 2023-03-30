f8869dc070c5e2b450af2a96e92991d8fd32d061
Your Name <iyanperezmarcos@gmail.com>
2023-03-30 09:40:59 +0200

    finished exercises

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const showPassword = ref(false);
+
+function changeVisibility() {
+  const passwordInput = document.getElementById('password');
+  passwordInput.type = showPassword.value ? 'password' : 'text';
+  showPassword.value = !showPassword.value;
+}
+</script>
+
+<template>
+    <div class="container text-center input-password">
+        <h1 class="my-3">Exercise Input Password</h1>
+        <input type="password" name="" id="password" class="form-control">
+        <div class="d-block">
+            <span id="show-password" class="mt-2 btn btn-primary" @click="changeVisibility()">
+                {{ showPassword ? 'Hide Password' : 'Show Password' }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    #show-password:hover {
+        cursor: pointer;
+        user-select: none;
+    }
+</style>
---
+++ src/components/ReadMore.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+    const showFullText = ref(false)
+
+</script>
+<template>
+  <div class="container text-center">
+    <h1 class="my-3">{{ title }}</h1>
+    <div>
+      <p class="mb-3 d-block" :class="{ 'cut-text': !showFullText }">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
+        molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
+        deserunt aperiam maxime consectetur recusandae quasi commodi dicta
+        temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
+        quaerat autem non hic, quidem dolore laboriosam.
+      </p>
+      <button class="btn btn-primary" @click="showFullText = !showFullText">
+        {{ showFullText ? 'Show Less' : 'Show More' }}
+      </button>
+    </div>
+    <hr class="mb-4" />
+  </div>
+</template>
+
+
+<style lang="css">
+  .cut-text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+</style>
